b905d4086df6e7a8833db50c95a421f1d5321929
leopavangomes <leonardo.gomes427@al.unieduk.com.br>
2026-04-14 22:24:26 -0300

    Create atividade.py

---
+++ AULA10-DICIONARIO/atividade.py
@@ -0,0 +1,13 @@
+Tabela = {"Maria": 25, 
+          "Ana": 50, 
+          "Carla": 35, 
+          "Neila": 44, 
+          "Erica": 18, 
+          "Ellen": 22, 
+          "Amanda": 26, 
+          "Neuza": 80, 
+          "Angela": 55, 
+          "Jessica": 30}
+
+for x, y in Tabela.items():
+    print(f"Nome: {x} ---- idade: {y}")
